0d19736e8b38ccdd48683b211f345b46517d9c52
Maksym Bevza <maksymbevza@gmail.com>
2016-10-05 15:29:37 +0300

    Sort EventProcessors

--- class_extractor2.py
+++ class_extractor2.py
@@ -1,16 +1,11 @@
 import collections
 import datetime
-import pandas as pd
 import record
-
-
-def inTimeRange(start_date, finish_date, date):
-    return start_date < date < finish_date
 
 
 def SkipTime(date):
     return datetime.date(date.year, date.month, date.day)
-    
+
 
 class LableRow:
     def __init__(self, action_id, start_t, finish_t):
@@ -22,34 +17,38 @@ class LableRow:
     def ParseLableRecord(cls, record):
         for index, row in record.frame.iterrows():
             action_id = int(row['action_id'])
-            start_t = datetime.datetime.strptime(row['start_t'], '%Y-%m-%d %H:%M:%S')
-            finish_t = datetime.datetime.strptime(row['finish_t'], '%Y-%m-%d %H:%M:%S')
-            
+            start_t = datetime.datetime.strptime(row['start_t'],
+                                                 '%Y-%m-%d %H:%M:%S')
+            finish_t = datetime.datetime.strptime(row['finish_t'],
+                                                  '%Y-%m-%d %H:%M:%S')
+
             yield cls(action_id, start_t, finish_t)
+
 
 class SensorRow:
     def __init__(self, time, data):
         self.time = time
         self.data = data
-        
+
     @classmethod
     def ParseSensorRecord(cls, record):
         for index, row in record.frame.iterrows():
-            time = record.startDate + datetime.timedelta(seconds=float(row['time']))
+            time = record.startDate + datetime.timedelta(
+                seconds=float(row['time']))
             yield cls(time, row)
-            
+
 
 class Event:
     def __init__(self, klass, time, data):
         self.klass = klass
         self.time = time
         self.data = data
-     
+
     def __lt__(self, o):
         return (self.time, self.klass) < (o.time, o.klass)
 
     def __str__(self):
-        return 'Event( klass: {}, time: {})'.format(self.klass, self.time)
+        return 'Event(klass: {}, time: {})'.format(self.klass, self.time)
 
 
 class EventProcessor:
@@ -77,62 +76,79 @@ class EventProcessor:
         self.events.sort()
 
     def WarmUp(self):
-        for record in self.lable_records:
-            self.FullfillLableEventsReal(record)
+        for rec in self.lable_records:
+            self.FullfillLableEventsReal(rec)
         self.lable_records[:] = []
 
-        for record in self.sensor_records:
-            self.FullfillSensorEventsReal(record)
+        for rec in self.sensor_records:
+            self.FullfillSensorEventsReal(rec)
         self.sensor_records[:] = []
 
         self.Sort()
 
     def __len__(self):
-#        return len(self.events)
         length = 0
         for records in [self.lable_records, self.sensor_records]:
-            for record in records:
-                length += len(record)
-        return length 
+            for rec in records:
+                length += len(rec)
+        return length
 
 
 class ClassExtractor:
     def __init__(self):
         self.nurse_date_to_events = collections.defaultdict(EventProcessor)
+        self.next_sample_id = 0
 
     def LoadLables(self, records):
-        for i, record in enumerate(records):
-            print('Parsing lable record {}/{}. Contains {} items.'.format(i + 1, len(records), len(record)))
-            event_processor = self.nurse_date_to_events[(record.nurseId, SkipTime(record.date))]
-            event_processor.FullfillLableEvents(record)
+        for i, rec in enumerate(records):
+            print('Parsing lable record {}/{}. Contains {} items.'.format(
+                i + 1,
+                len(records),
+                len(rec)))
+            event_processor = self.nurse_date_to_events[(rec.nurseId,
+                                                         SkipTime(rec.date))]
+            event_processor.FullfillLableEvents(rec)
 
     def LoadSensors(self, records):
-        for i, record in enumerate(records):
-            print('Parsing sensor record {}/{}. Contains {} items.'.format(i + 1, len(records), len(record)))
-            event_processor = self.nurse_date_to_events[(record.nurseId, SkipTime(record.startDate))]
-            event_processor.FullfillSensorEvents(record)
+        for i, rec in enumerate(records):
+            print('Parsing sensor record {}/{}. Contains {} items.'.format(
+                i + 1,
+                len(records),
+                len(rec)))
+            date = rec.startDate
+            event_processor = self.nurse_date_to_events[(rec.nurseId,
+                                                         SkipTime(date))]
+            event_processor.FullfillSensorEvents(rec)
 
-    def Sort(self):
-        for i, (nurse_date, event_processor) in enumerate(self.nurse_date_to_events.items()):
-            print('Sorting EventProcessor {}/{}. Contains {} items.'.format(i + 1, len(self.nurse_date_to_events),
-                                                                            len(event_processor)))
+    def NextSampleId(self):
+        self.next_sample_id += 1
+        return self.next_sample_id - 1
 
     def Process(self):
-        next_sample_id = 0
+        total_len = sum([len(ep) for ep in self.nurse_date_to_events.values()])
 
-        total_length = sum([len(ep) for ep in self.nurse_date_to_events.values()])
-
-        for i, (nurse_date, event_processor) in enumerate(self.nurse_date_to_events.items()):
-            print('Processing EventProcessor {}/{} containing {}/{} items.'.format(i + 1, len(self.nurse_date_to_events),
-                                                                                   len(event_processor), total_length))
+        nurse_date_to_events = list(self.nurse_date_to_events.items())
+        nurse_date_to_events.sort(key=lambda x: len(x[1]))
+        del self.nurse_date_to_events
+        for i, (nurse_date, event_processor) in enumerate(nurse_date_to_events):
+            print(
+                'Processing EventProcessor {}/{}. Contains {}/{} items.'.format(
+                    i + 1,
+                    len(nurse_date_to_events),
+                    len(event_processor),
+                    total_len))
 
             event_processor.WarmUp()
             sample_builders = {}
             for event in event_processor.events:
                 data = event.data
+                if event.klass and data.action_id >= 100:
+                    continue
+
                 if event.klass == -1:
-                    sample_builders[data] = record.TrainingSampleBuilder(next_sample_id, data.action_id)
-                    next_sample_id += 1
+                    sample_builders[data] = record.TrainingSampleBuilder(
+                        self.NextSampleId(),
+                        data.action_id)
                 elif event.klass == +1:
                     sample = sample_builders.pop(data).build()
                     sample.write()
@@ -140,5 +156,5 @@ class ClassExtractor:
                     for builder in sample_builders.values():
                         builder.add_row(data.data)
 
-            print('Erasing EventProcessor.'.format(len(event_processor)))
-            self.nurse_date_to_events[nurse_date] = None
+            print('Erasing EventProcessor.')
+            nurse_date_to_events[i] = None
